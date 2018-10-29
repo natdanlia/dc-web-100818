2c74b4d6718e56466da660f4db37ae3130e06265
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-10-29 11:03:59 -0400

    prelecture 13

--- 11-intro-to-testing/palindrome.rb
+++ 11-intro-to-testing/palindrome.rb
@@ -2,13 +2,19 @@ require 'pry'
 
 class PalindromeChecker
 
-    # def is_palindrome?(str)
-    #     if str.length <= 1
-    #         return true
-    #     elsif str[0] != str[-1]
-    #         return false
-    #     else
-    #         return is_palindrome?(str[1..-2])
-    #     end
-    # end
+    def is_palindrome?(str)
+        if str.class != String
+            raise ArgumentError
+        end
+        str.downcase!
+        str.gsub!(/[\W]/, "")
+        if str.length <= 1
+            return true
+        end
+        if str[0] == str[-1]
+            return is_palindrome?(str[1...-1])
+        else
+            return false
+        end
+    end
 end
