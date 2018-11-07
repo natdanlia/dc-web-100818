d8c8db2b915ecdb6c5d58aeac24cd0eef44928ca
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-11-07 14:23:41 -0500

    pre lecture 18

---
+++ 18-rails-associations/snack-tracker/db/seeds.rb
@@ -0,0 +1,5 @@
+Snack.destroy_all
+
+Snack.create(name: "Nachos", deliciousness: 3, calories: 600)
+Snack.create(name: "Mozzarella Sticks", deliciousness: 6, calories: 500)
+Snack.create(name: "Sliders", deliciousness: 8, calories: 650)
