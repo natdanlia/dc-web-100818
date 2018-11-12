58761c401b731d6b3375ee47ad6107b64c5f7091
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-11-12 09:43:27 -0500

    prelecture 19

---
+++ 19-rails-validations/snack-tracker/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "/foods/test", to: "snacks#test", as: "the_test"
+  root "snacks#home"
+  resources :snacks
+  resources :retailers, only: [:index, :show, :new, :create]
+end
